b4202e1f442884879568d176967713397445b2f4
nr <99275945+giterhaber@users.noreply.github.com>
2022-12-10 05:10:36 +0800

    Create notification.js

---
+++ notification.js
@@ -0,0 +1,23 @@
+function notifyMe() {
+
+  let accessGranted = "Notification Ready"
+  if (!("Notification" in window)) {
+    alert("This browser does not support desktop notification");
+  } else if (Notification.permission === "granted") {
+    const notification = new Notification('THE CONTENT');
+    notification.onclick = (event) => {
+    let url = window.location.href
+    window.open(url, '_blank');
+    }
+    // …
+  } else if (Notification.permission !== "denied") {
+    Notification.requestPermission().then((permission) => {
+      if (permission === "granted") {
+        const notification = new Notification(accessGranted)
+      }
+    });
+  }
+
+}
+
+//setTimeout(notifyMe, 2000)
