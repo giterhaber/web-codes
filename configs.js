363abcc3745107dbe1f11dd1374efe2e543c37dc
nr <99275945+giterhaber@users.noreply.github.com>
2022-12-10 07:04:49 +0800

    config

---
+++ configs.js
@@ -0,0 +1,9 @@
+export const karlo_gogong = {
+    apiKey: "AIzaSyDAk6wPmjinvA2AaKkBpXFpOkEwXssluvc",
+    authDomain: "chainresolver.firebaseapp.com",
+    projectId: "chainresolver",
+    storageBucket: "chainresolver.appspot.com",
+    messagingSenderId: "28247484124",
+    appId: "1:28247484124:web:af0fc65ac1c51600b1b619"
+}
+
