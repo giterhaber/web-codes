c3010d53b1a679b7e564ffb84556e8d8e2a58d02
nr <99275945+giterhaber@users.noreply.github.com>
2022-12-10 13:46:07 +0800

    import firebase

---
+++ importFirebase.js
@@ -0,0 +1,3 @@
+import firebase from "https://cdn.skypack.dev/firebase/compat/app";
+import "https://cdn.skypack.dev/firebase/compat/auth";
+import "https://cdn.skypack.dev/firebase/compat/firestore";
