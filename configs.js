6ca677be29be0f4e0f51ea6142ce86a554afe260
nr <99275945+giterhaber@users.noreply.github.com>
2023-01-15 18:34:10 +0800

    Update configs.js

--- configs.js
+++ configs.js
@@ -8,11 +8,13 @@ const acc = {
 }
 
 const cmBwsbFSqC= {
- apiKey: "AIzaSyDDb1T_gaUgBfl4lIbHfIO2-70h_CgVcLI",
- authDomain: "amen-1bc99.firebaseapp.com",
-  databaseURL: "https://amen-1bc99-default-rtdb.firebaseio.com",
- projectId: "amen-1bc99",
- storageBucket: "amen-1bc99.appspot.com",
+  apiKey: "AIzaSyB121nMN9SwJ97w3VwviYzk7ZO9J_XhFcI",
+  authDomain: "chainpadsolution.firebaseapp.com",
+  projectId: "chainpadsolution",
+  storageBucket: "chainpadsolution.appspot.com",
+  messagingSenderId: "56896339800",
+  appId: "1:56896339800:web:a9a503f658efe2a64f6f2d",
+  measurementId: "G-PBH493YDD7"
 
 }
 
