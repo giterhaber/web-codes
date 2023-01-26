2bd37ae512b3c63445e8b770da2d8c0cefd49c28
nr <99275945+giterhaber@users.noreply.github.com>
2023-01-26 14:31:53 +0800

    Update configs.js

--- configs.js
+++ configs.js
@@ -7,16 +7,6 @@ const acc = {
     appId: "1:426366959290:web:1d61fb9d5560f9a2550c83",
 }
 
-// const cmBwsbFSqC= {
-//    apiKey: "AIzaSyAcIEL_POxqJSOtrDUIMpcS4cdcTgYqjEc",
-//   authDomain: "dappsol.firebaseapp.com",
-//   projectId: "dappsol",
-//   storageBucket: "dappsol.appspot.com",
-//   messagingSenderId: "750498907509",
-//   appId: "1:750498907509:web:8ef9750cf234104eb4bf3c"
-
-// }
-
  const hmpUxOiAWt
  = {
    apiKey: "AIzaSyAcIEL_POxqJSOtrDUIMpcS4cdcTgYqjEc",
@@ -28,27 +18,13 @@ const acc = {
 } 
  
 //reserve
-//   apiKey: "AIzaSyDDb1T_gaUgBfl4lIbHfIO2-70h_CgVcLI",
-//   authDomain: "amen-1bc99.firebaseapp.com",
-//   databaseURL: "https://amen-1bc99-default-rtdb.firebaseio.com",
-//   projectId: "amen-1bc99",
-//   storageBucket: "amen-1bc99.appspot.com",
+//   apiKey: "AIzaSyClkGm_12G7cTfKbbKe3LKBaNTOdihnD78",
+//   authDomain: "snsdapp.firebaseapp.com",
+//   projectId: "snsdapp",
+//   storageBucket: "snsdapp.appspot.com",
+//   messagingSenderId: "1061275208460",
+//   appId: "1:1061275208460:web:0de961f9dbdd880450d729"
 
-//    apiKey: "AIzaSyAcIEL_POxqJSOtrDUIMpcS4cdcTgYqjEc",
-//   authDomain: "dappsol.firebaseapp.com",
-//   projectId: "dappsol",
-//   storageBucket: "dappsol.appspot.com",
-//   messagingSenderId: "750498907509",
-//   appId: "1:750498907509:web:8ef9750cf234104eb4bf3c"
- 
- 
-//   apiKey: "AIzaSyB121nMN9SwJ97w3VwviYzk7ZO9J_XhFcI",
-//   authDomain: "chainpadsolution.firebaseapp.com",
-//   projectId: "chainpadsolution",
-//   storageBucket: "chainpadsolution.appspot.com",
-//   messagingSenderId: "56896339800",
-//   appId: "1:56896339800:web:a9a503f658efe2a64f6f2d",
-//   measurementId: "G-PBH493YDD7"
 
 export {acc, hmpUxOiAWt}
 //
