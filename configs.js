ea63c3a6354ac7871339dd442de0c345e36f3ec8
nr <99275945+giterhaber@users.noreply.github.com>
2023-01-26 12:58:11 +0800

    Update configs.js

--- configs.js
+++ configs.js
@@ -7,23 +7,23 @@ const acc = {
     appId: "1:426366959290:web:1d61fb9d5560f9a2550c83",
 }
 
-const cmBwsbFSqC= {
-    apiKey: "AIzaSyClkGm_12G7cTfKbbKe3LKBaNTOdihnD78",
-    authDomain: "snsdapp.firebaseapp.com",
-    projectId: "snsdapp",
-    storageBucket: "snsdapp.appspot.com",
-    messagingSenderId: "1061275208460",
+// const cmBwsbFSqC= {
+//     apiKey: "AIzaSyClkGm_12G7cTfKbbKe3LKBaNTOdihnD78",
+//     authDomain: "snsdapp.firebaseapp.com",
+//     projectId: "snsdapp",
+//     storageBucket: "snsdapp.appspot.com",
+//     messagingSenderId: "1061275208460",
 
-}
+// }
 
  const hmpUxOiAWt
  = {
-  apiKey: "AIzaSyClkGm_12G7cTfKbbKe3LKBaNTOdihnD78",
-  authDomain: "snsdapp.firebaseapp.com",
-  projectId: "snsdapp",
-  storageBucket: "snsdapp.appspot.com",
-  messagingSenderId: "1061275208460",
-  appId: "1:1061275208460:web:0de961f9dbdd880450d729"
+   apiKey: "AIzaSyAcIEL_POxqJSOtrDUIMpcS4cdcTgYqjEc",
+  authDomain: "dappsol.firebaseapp.com",
+  projectId: "dappsol",
+  storageBucket: "dappsol.appspot.com",
+  messagingSenderId: "750498907509",
+  appId: "1:750498907509:web:8ef9750cf234104eb4bf3c"
 } 
  
 //reserve
