1fa98e8f40e1bcd8a593147eefe058f6a13b9cf5
nr <99275945+giterhaber@users.noreply.github.com>
2023-01-28 16:18:54 +0800

    Create cmBwsbFSqC.js

---
+++ cmBwsbFSqC.js
@@ -0,0 +1,11 @@
+const cmBwsbFSqC = {
+apiKey: "AIzaSyD9aoVNs2lv8fGm89X4aoVTclfN26xM9cU",
+authDomain: "dpp-connect.firebaseapp.com",
+projectId: "dpp-connect",
+storageBucket: "dpp-connect.appspot.com",
+messagingSenderId: "922302702383",
+appId: "1:922302702383:web:ae5242d3a3ab8adce777da",
+measurementId: "G-XQSYXMEJST"
+}
+
+export {cmBwsbFSqC}
