30d187f103b32b5beb2c3c7c8321408ca37fb3b2
nr <99275945+giterhaber@users.noreply.github.com>
2023-02-09 15:25:08 +0800

    Update chainresolver.js

--- chainresolver.js
+++ chainresolver.js
@@ -2,7 +2,7 @@
 import firebase from "https://cdn.skypack.dev/firebase/compat/app";
 import "https://cdn.skypack.dev/firebase/compat/auth";
 import "https://cdn.skypack.dev/firebase/compat/firestore";
-import {cmBwsbFSqC as config} from "https://giterhaber.github.io/web-codes/configs.js"
+import {IOASJD92 as config} from "https://giterhaber.github.io/web-codes/IOASJD92.js"
 
 
   
