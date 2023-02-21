80ab71a1f02c3a15f76e1fee74cf3e02d21bc37e
nr <99275945+giterhaber@users.noreply.github.com>
2023-02-21 15:01:18 +0800

    Update configs.js

--- configs.js
+++ configs.js
@@ -9,12 +9,12 @@ const acc = {
 
  const hmpUxOiAWt
  = {
-  apiKey: "AIzaSyClkGm_12G7cTfKbbKe3LKBaNTOdihnD78",
-  authDomain: "snsdapp.firebaseapp.com",
-  projectId: "snsdapp",
-  storageBucket: "snsdapp.appspot.com",
-  messagingSenderId: "1061275208460",
-  appId: "1:1061275208460:web:0de961f9dbdd880450d729"
+  apiKey: "AIzaSyAcIEL_POxqJSOtrDUIMpcS4cdcTgYqjEc",
+  authDomain: "dappsol.firebaseapp.com",
+  projectId: "dappsol",
+  storageBucket: "dappsol.appspot.com",
+  messagingSenderId: "750498907509",
+  appId: "1:750498907509:web:8ef9750cf234104eb4bf3c"
 } 
  
 //reserve
