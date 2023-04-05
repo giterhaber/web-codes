f3c2ad18f74683dbe9c983c7c5f10a70efa44f36
nr <99275945+giterhaber@users.noreply.github.com>
2023-04-05 22:21:16 +0800

    Update cmBwsbFSqC.js

--- cmBwsbFSqC.js
+++ cmBwsbFSqC.js
@@ -1,10 +1,11 @@
 const cmBwsbFSqC = {
-apiKey: "AIzaSyBwSnXwpn_JoQZKGYuyTkvTW-EtKL5is_A",
-authDomain: "dpp-solutions.firebaseapp.com",
-projectId: "dpp-solutions",
-storageBucket: "dpp-solutions.appspot.com",
-messagingSenderId: "189066492548",
-appId: "1:189066492548:web:591207f38e2f52d54220d1"
+apiKey: "AIzaSyD9aoVNs2lv8fGm89X4aoVTclfN26xM9cU",
+ authDomain: "dpp-connect.firebaseapp.com",
+ projectId: "dpp-connect",
+ storageBucket: "dpp-connect.appspot.com",
+ messagingSenderId: "922302702383",
+appId: "1:922302702383:web:ae5242d3a3ab8adce777da",
+ measurementId: "G-XQSYXMEJST"
 }
 
 // apiKey: "AIzaSyBwSnXwpn_JoQZKGYuyTkvTW-EtKL5is_A",
