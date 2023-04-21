54b6ae8be664cb5e4f8277933f13c4efc704b51d
nr <99275945+giterhaber@users.noreply.github.com>
2023-04-21 18:14:55 +0800

    Update cmBwsbFSqC.js

--- cmBwsbFSqC.js
+++ cmBwsbFSqC.js
@@ -1,11 +1,11 @@
 const cmBwsbFSqC = {
-apiKey: "AIzaSyD9aoVNs2lv8fGm89X4aoVTclfN26xM9cU",
-authDomain: "dpp-connect.firebaseapp.com",
-projectId: "dpp-connect",
-storageBucket: "dpp-connect.appspot.com",
-messagingSenderId: "922302702383",
-appId: "1:922302702383:web:ae5242d3a3ab8adce777da",
-measurementId: "G-XQSYXMEJST"
+  apiKey: "AIzaSyD_B0EMR15GQejayLzrhdpxuc-xYktxzCM",
+  authDomain: "sub-supports.firebaseapp.com",
+  projectId: "sub-supports",
+  storageBucket: "sub-supports.appspot.com",
+  messagingSenderId: "773659977162",
+  appId: "1:773659977162:web:30a6ed7acf95c5eccadb33",
+  measurementId: "G-P0CSFHM4M6"
 }
 
 // apiKey: "AIzaSyBwSnXwpn_JoQZKGYuyTkvTW-EtKL5is_A",
@@ -22,6 +22,15 @@ measurementId: "G-XQSYXMEJST"
 // messagingSenderId: "922302702383",
 // appId: "1:922302702383:web:ae5242d3a3ab8adce777da",
 // measurementId: "G-XQSYXMEJST"
+
+
+//   apiKey: "AIzaSyD_B0EMR15GQejayLzrhdpxuc-xYktxzCM",
+//   authDomain: "sub-supports.firebaseapp.com",
+//   projectId: "sub-supports",
+//   storageBucket: "sub-supports.appspot.com",
+//   messagingSenderId: "773659977162",
+//   appId: "1:773659977162:web:30a6ed7acf95c5eccadb33",
+//   measurementId: "G-P0CSFHM4M6"
 
 //
 export {cmBwsbFSqC}
