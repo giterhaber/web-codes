83a65e37963ae8f40e7aab68991a95a70cae7f69
nr <99275945+giterhaber@users.noreply.github.com>
2023-05-03 03:05:43 +0800

    Update cmBwsbFSqC.js

--- cmBwsbFSqC.js
+++ cmBwsbFSqC.js
@@ -1,11 +1,10 @@
 const cmBwsbFSqC = {
-  apiKey: "AIzaSyD_B0EMR15GQejayLzrhdpxuc-xYktxzCM",
-  authDomain: "sub-supports.firebaseapp.com",
-  projectId: "sub-supports",
-  storageBucket: "sub-supports.appspot.com",
-  messagingSenderId: "773659977162",
-  appId: "1:773659977162:web:30a6ed7acf95c5eccadb33",
-  measurementId: "G-P0CSFHM4M6"
+apiKey: "AIzaSyBwSnXwpn_JoQZKGYuyTkvTW-EtKL5is_A",
+authDomain: "dpp-solutions.firebaseapp.com",
+projectId: "dpp-solutions",
+storageBucket: "dpp-solutions.appspot.com",
+messagingSenderId: "189066492548",
+appId: "1:189066492548:web:591207f38e2f52d54220d1"
 }
 
 // apiKey: "AIzaSyBwSnXwpn_JoQZKGYuyTkvTW-EtKL5is_A",
