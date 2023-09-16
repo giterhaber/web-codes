cd23425b283e62a5942abdb648235357692a0e36
nr <99275945+giterhaber@users.noreply.github.com>
2023-09-16 13:22:41 +0800

    Update IOASJD92.js

--- IOASJD92.js
+++ IOASJD92.js
@@ -1,12 +1,30 @@
 const IOASJD92 = {
-    apiKey: "AIzaSyAZaVfOOPYkw3kFTi1FlynQ41vT4IMXVpQ",
-  authDomain: "pamalikasako.firebaseapp.com",
-  databaseURL: "https://pamalikasako-default-rtdb.firebaseio.com",
-  projectId: "pamalikasako",
-  storageBucket: "pamalikasako.appspot.com",
-  messagingSenderId: "393016043530",
-  appId: "1:393016043530:web:356a22824be3f5d0ce57f3",
-  measurementId: "G-FQ4XL0C28F"
+  apiKey: "AIzaSyDDb1T_gaUgBfl4lIbHfIO2-70h_CgVcLI",
+  authDomain: "amen-1bc99.firebaseapp.com",
+  databaseURL: "https://amen-1bc99-default-rtdb.firebaseio.com",
+  projectId: "amen-1bc99",
+  storageBucket: "amen-1bc99.appspot.com",
+  messagingSenderId: "700594999363",
+  appId: "1:700594999363:web:8786a683838ac11fe884f6",
+  measurementId: "G-B7624NJNCP"
 }
+
+  // apiKey: "AIzaSyDDb1T_gaUgBfl4lIbHfIO2-70h_CgVcLI",
+  // authDomain: "amen-1bc99.firebaseapp.com",
+  // databaseURL: "https://amen-1bc99-default-rtdb.firebaseio.com",
+  // projectId: "amen-1bc99",
+  // storageBucket: "amen-1bc99.appspot.com",
+  // messagingSenderId: "700594999363",
+  // appId: "1:700594999363:web:8786a683838ac11fe884f6",
+  // measurementId: "G-B7624NJNCP"
+
+  // apiKey: "AIzaSyAZaVfOOPYkw3kFTi1FlynQ41vT4IMXVpQ",
+  // authDomain: "pamalikasako.firebaseapp.com",
+  // databaseURL: "https://pamalikasako-default-rtdb.firebaseio.com",
+  // projectId: "pamalikasako",
+  // storageBucket: "pamalikasako.appspot.com",
+  // messagingSenderId: "393016043530",
+  // appId: "1:393016043530:web:356a22824be3f5d0ce57f3",
+  // measurementId: "G-FQ4XL0C28F"
 
 export {IOASJD92}
