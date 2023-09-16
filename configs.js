463599c7e6c385f3f4b7ebec0df87caf430de925
nr <99275945+giterhaber@users.noreply.github.com>
2023-09-16 19:17:05 +0800

    Update configs.js

--- configs.js
+++ configs.js
@@ -9,15 +9,15 @@ const acc = {
 
  const hmpUxOiAWt
  = {
-  apiKey: "AIzaSyClkGm_12G7cTfKbbKe3LKBaNTOdihnD78",
-  authDomain: "snsdapp.firebaseapp.com",
-  projectId: "snsdapp",
-  storageBucket: "snsdapp.appspot.com",
-  messagingSenderId: "1061275208460",
-  appId: "1:1061275208460:web:0de961f9dbdd880450d729"
+  apiKey: "AIzaSyD69bgj4MFshniXYfkXyaNVQZirGCXLTVA",
+  authDomain: "krkrkrkr-ebbd1.firebaseapp.com",
+  projectId: "krkrkrkr-ebbd1",
+  storageBucket: "krkrkrkr-ebbd1.appspot.com",
+  messagingSenderId: "49644818512",
+  appId: "1:49644818512:web:6da9f88eab7e8013f28155"
 } 
  
-//reserve
+//WALA NANING DUHA
 //   apiKey: "AIzaSyClkGm_12G7cTfKbbKe3LKBaNTOdihnD78",
 //   authDomain: "snsdapp.firebaseapp.com",
 //   projectId: "snsdapp",
