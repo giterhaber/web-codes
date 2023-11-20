4e5a0c4ffa46f61834df5351df0e7f8e20d430e9
nr <99275945+giterhaber@users.noreply.github.com>
2023-11-21 03:07:32 +0800

    Create J2Nggu20.js

---
+++ J2Nggu20.js
@@ -0,0 +1,10 @@
+const J2Nggu20 = {
+  apiKey: "AIzaSyBMkoF4bhtxrZpyOCCPBpIeZ7MZuD2VyAE",
+  authDomain: "jangjangdatabase.firebaseapp.com",
+  projectId: "jangjangdatabase",
+  storageBucket: "jangjangdatabase.appspot.com",
+  messagingSenderId: "301224049741",
+  appId: "1:301224049741:web:8eacece5f15bf48a29f004"
+}
+
+export {J2Nggu20}
